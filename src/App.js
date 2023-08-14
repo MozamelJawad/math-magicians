e238db5c2120f8da1e37d29d95360a709f7345cd
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-14 22:38:48 +0430

    create react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to my first React Project.
+        </p>
+        <a
+          className="App-link"
+        >
+          Math Magicians
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
