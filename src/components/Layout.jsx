bb9c16ee06c8f5a51be88e723bfc5d774a312053
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-23 13:44:53 +0430

    add router layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Layout = () => (
+  <div className="wrapper">
+    <Navbar />
+    <Outlet />
+  </div>
+);
+
+export default Layout;
