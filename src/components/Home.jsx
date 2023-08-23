67373e1a358d031d516198f844f6268f3fdfb1e4
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-23 13:45:43 +0430

    add home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="HomeMain">
+      <h2 className="TopHeading">Welcome to Math Magician.</h2>
+      <div>
+        <p className="para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </span>
+          <span>
+            Possimus temporibus libero molestiae enim, beatae odio reprehenderit iusto vero
+          </span>
+          laudantium nemo obcaecati fuga error mollitia labore fugit a sequi dolore ullam?
+        </p>
+        <p className="para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </span>
+          <span>
+            Possimus temporibus libero molestiae enim, beatae odio reprehenderit iusto vero
+          </span>
+          laudantium nemo obcaecati fuga error mollitia labore fugit a sequi dolore ullam?
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
